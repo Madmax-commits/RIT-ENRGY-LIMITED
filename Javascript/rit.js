0b8bc6d42944c8a137f52a73d8045f328cd35210
Madmax-commits <tobamicar@gmail.com>
2025-07-22 13:25:22 +0100

    js

--- Javascript/rit.js
+++
@@ -1,9 +0,0 @@
-const lastUpdated = new Date(document.lastModified);
-document.getElementById("last-updated").textContent =
-  `Last updated: ${lastUpdated.toLocaleString()}`;
-
- function toggleMenu() {
-  const menu = document.getElementById("mobileMenu");
-  menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
-
